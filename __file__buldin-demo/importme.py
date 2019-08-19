f3b9a3b2615b0ba0ea51c20c392ed5dbc43cac21
nicolaosoikou@gmail.com <nicolaosoikou@gmail.com>
2019-08-19 19:32:34 +0200

    Added __file__ example

---
+++ __file__buldin-demo/importme.py
@@ -0,0 +1,1 @@
+print(1, '__file__ when importme.py gets imported:', __file__)
---
+++ __file__buldin-demo/runme.py
@@ -0,0 +1,3 @@
+import importme
+print(2, '__file__ when executing runme.py:', __file__)
+print(3, 'importme.__file__:', importme.__file__)
